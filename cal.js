ecc860fa8ecda3fc1199ca9641d20845488e33a3
priya <riaharshini1998@gmail.com>
2020-12-12 10:21:15 -0500

    change

--- cal.js
+++ cal.js
@@ -1,13 +1,25 @@
-display=(val) => 
-{
-   document.getElementById("result").value += val
+const number=[1,2,3,4,5,6,7,8,9,0];
+const operations=["+","-","*","/","="];
+getButtons=(val) => {
+return `<button type="button" class="btn btn-danger" style="margin: 10px;" id=${val} value=${val}>${val}</button>`;
 }
-solution=() =>
-{
-    let x= document.getElementById("result").value 
-    let y= eval(x)
-    document.getElementById("result").value= y
+buttons=(val)=>{
+    val.forEach((val)=>{
+        document.getElementById("calculator").innerHTML += getButtons(val);
+    });
+};
+
+document.getElementById("calculator").addEventListener("click",Element1=(t)=>{
+    if(t.target.value !== "="){
+        document.getElementById("inputtext").value += t.target.value;
+    }
+    else{
+        document.getElementById("inputtext").value=eval(document.getElementById("inputtext").value)
+    }
+})
+
+clearValue= () => {
+    document.getElementById("inputtext").value=" "
 }
-clearscreen=() =>{
-    document.getElementById("result").value=" "
-}
+buttons(number);
+buttons(operations)
